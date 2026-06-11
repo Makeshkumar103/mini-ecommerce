2b0adc5a8a7e00457a90c015dcdc4f514a07ed44
Makeshkumar M <152783179+Makeshkumar103@users.noreply.github.com>
2026-06-11 16:22:07 +0530

    changed files according to react router. And listed the product details in home component

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,11 +1,23 @@
+import { Link } from "react-router-dom";
+
 export default function ProductCard ( {product} ) {
+
+    const imgSrc = product?.images?.[0]?.image || "/images/no-image-placeholder.png";
+
+    const handleImageError = (e) => {
+        e.target.src = "/images/no-image-placeholder.png";
+    }
+
     return (
         <div className="col-sm-12 col-md-6 col-lg-3 my-3">
                 <div className="card p-3 rounded">
+                    {/* <div> {product.description}</div> */}
+
                     <img
                         className="card-img-top mx-auto"
-                        src={product.images[0].image}
+                        src={imgSrc}
                         alt="product-image"
+                        onError={handleImageError}
                         />  
                     <div className="card-body d-flex flex-column">
                         <h5 className="card-title">
@@ -14,11 +26,11 @@ export default function ProductCard ( {product} ) {
                         </h5>
                     <div className="ratings mt-auto">
                         <div className="rating-outer">
-                            <div className="rating-inner"></div>
+                            <div className="rating-inner" style={{width : `${product.ratings/5 *100}%`}}></div>
                         </div>
                     </div>
-                    <p className="card-text">$245.67</p>
-                        <a href="*" id="view_btn" className="btn btn-block">View Details</a>
+                    <p className="card-text">{product.price}</p>
+                        <Link to={'/prouct/+product._id'} id="view_btn" className="btn btn-block">View Details</Link>
                     </div>
                 </div>
             </div>
